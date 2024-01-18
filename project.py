9bacb617c4d903a2aa145d18765763061f8c2a2b
ArthurKR <arturs.krievans@gmail.com>
2024-01-18 02:37:44 +0200

    Parveidots kods no selenium uz bs4

--- project.py
+++ project.py
@@ -1,107 +1,177 @@
-import selenium
-import csv
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-import time
-from openpyxl import Workbook, load_workbook 
-
-
-service = Service()
-option = webdriver.ChromeOptions()
-##option.add_argument("--headless")
-driver = webdriver.Chrome(service=service, options=option)
+import requests
+from bs4 import BeautifulSoup
+import random
 
 url = "https://codeforces.com/problemset"
-user_url = input("Provide your codeforces USERNAME or FULL LINK: ")
-url_beginning = "https://codeforces.com/profile/"
-if (url_beginning not in user_url):
-    user_url = url_beginning + user_url
+response = requests.get(url)
 
-name = user_url[len(url_beginning):]
+soup = BeautifulSoup(response.text, 'html.parser')
 
+options = soup.find_all('option')
 
-driver.get(url)
-
-
-tag_options = driver.find_element(By.TAG_NAME, "select")
-tag_elements = [x for x in tag_options.find_elements(By.TAG_NAME, "option")] 
 
 tag_names = {}
-for item in tag_elements:
-    tag = item.get_attribute("value")
-    if tag is not None and tag != "" and tag != "combine-tags-by-or":
-        tag_names[tag] = 0;
+success_rate = {}
+for option in options:
+    tag = option.get('value')
+    if tag != "" and tag != "combine-tags-by-or":
+        tag_names[tag] = 0
+        success_rate[tag] = 0
+    
 
+url_beginning = "https://codeforces.com/profile/"
+username = input("Provide your codeforces USERNAME or FULL LINK: ")
 
+if url_beginning not in username:
+    username = url_beginning + username
 
-driver.get(user_url)
+name = username[len(url_beginning):]
 
+response = requests.get(username)
+actual_url = response.url
 
-if (driver.current_url == "https://codeforces.com/"):
-    print("The provided uersname does not exist! Please run the program again and enter a valid/existing username.")
-    driver.quit()
+if actual_url != username:
+    print("The name you provided is not correct or does not exist! Please run the program again and provide a valid username.")
     exit()
 
-profile_overview = driver.find_element(By.CLASS_NAME, "info")
-rating = 0
-for line in profile_overview.text.splitlines():
-    if "Contest rating" in line:
-        rating = int(line.split()[2])
+soup = BeautifulSoup(response.text, 'html.parser')
+div_container = soup.find(class_="info")
+li_elements = div_container.find_all('span', style = "font-weight:bold;")
 
+rating = 0
+if len(li_elements) > 0:
+    rating = li_elements[0].text
+
+rating = int(rating)
 rating_lowerbound = min(3000, max(800, rating - 200 - rating%100))
 rating_upperbound = min(3500, max(1200, rating + 400 - rating%100))
 
 
-driver.get("https://codeforces.com/submissions/" + name)
+all_problems = set()
+cur_page = 1
 
-solved_problems = set()
-difficulty = 0
+total_difficulty = 0
+difficulty_count = 0
 
-for i in range(1, 3):
+while True:
 
-    submission_url = "https://codeforces.com/submissions/" + name + "/page/" + str(i)
-    driver.get(submission_url)
+    cur_url =  "https://codeforces.com/submissions/" + name + "/page/" + str(cur_page)
+    response = requests.get(cur_url)
 
-    if driver.current_url == "https://codeforces.com/":
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    successful_submission = False
+
+    submission_data = soup.find_all("td")
+    for cell in submission_data:
+        parsed = cell.text.replace("  ", "").replace("\n", "")
+        if "Accepted" in parsed:
+            successful_submission = True
+ 
+    
+  
+    sub_count = 0
+    all_links = soup.find(class_="datatable").find_all("a")
+    for link in all_links:
+        href_value = link.get('href')
+        if "contest" and "problem" in href_value:
+            build_link = "https://codeforces.com" + href_value
+            if build_link not in all_problems:
+
+                response = requests.get(build_link)
+                soup = BeautifulSoup(response.text, 'html.parser')
+
+                tags = soup.find_all("span", class_="tag-box")
+                for tag in tags:
+
+                    parsed = tag.text.replace("\n", "").replace("  ", "")
+                    filtered = [x for x in tag_names.keys() if x in parsed]
+
+                    for x in filtered:
+                        tag_names[x] += 1
+                        if successful_submission:
+                            success_rate[x] += 1
+                        
+                    digits = [x for x in parsed if x >= "0" and x <= "9"]
+                    if len(digits) > 0:
+                        total_difficulty += int("".join(digits))
+                        difficulty_count  += 1
+    
+                   
+
+                all_problems.add(build_link)
+            sub_count += 1
+        
+   
+    if sub_count < 50:
         break
 
-    submission_status = driver.find_elements(By.TAG_NAME, "tr")
-
-    for submission in submission_status:
-        if "Accepted" in submission.text:
-            href_tags = submission.find_elements(By.TAG_NAME, "a")
-            for tag in href_tags:
-                if "contest" and "problem" in tag.get_attribute("href"):
-                    solved_problems.add(tag.get_attribute("href"))
-      
-problem_count = len(solved_problems)
-
-
-for problem_link in solved_problems:
-
-    driver.get(problem_link)
-    current_tags = driver.find_elements(By.CLASS_NAME, "tag-box")
-    difficulty_found = False
-    for tag in current_tags:
-        if tag.text in tag_names.keys():
-            tag_names[tag.text] += 1
-        else:
-            difficulty += int(tag.text[1:]) if tag.text[0]=="*" else int(tag.text)
-            difficulty_found = True
-
-    if not difficulty_found:
-        problem_count -= 1
+    cur_page += 1
 
 
 
-if problem_count == 0 and len(solved_problems) > 0:
-    print("Difficulty is not yet determined")
-elif problem_count == 0:
-    print("User has not solved any problems.")
-else:
-    print("Average Difficulty: ", difficulty//problem_count)
+avg_rate = 0
+rate_count = 0
+avg_tag_frequency = 0
 
-for tags in tag_names:
-    print(tags, tag_names[tags])
+for key in tag_names:
+    avg_tag_frequency += tag_names[key]
+    if tag_names[key] == 0:
+        continue
+    success_rate[key] = success_rate[key]/tag_names[key]*100
+    avg_rate += success_rate[key]
+    rate_count += 1
+
+avg_rate /= rate_count
+avg_tag_frequency /= len(tag_names)
+
+
+problem_url = "https://codeforces.com/problemset/?tags={}-{}".format(rating_lowerbound, rating_upperbound)
+response = requests.get(problem_url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+print(rating_lowerbound, rating_upperbound)
+
+last_page = int(soup.find_all("span", class_="page-index")[-1].text)
+recommended_problems = []
+
+
+valid_length = True
+while valid_length:
+    page = random.randint(1, last_page)
+    problem_url = "https://codeforces.com/problemset/page/{}?tags={}-{}".format(page, rating_lowerbound, rating_upperbound)
+    response = requests.get(problem_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    td_con = soup.find_all("td")
+    for div in td_con:
+        for elements in div.find_all("div", style="float: right; font-size: 1.1rem; padding-top: 1px; text-align: right;"):
+            for li in elements.find_all("a"):
+                if li.text == "*special problem":
+                    continue
+            
+                if success_rate[li.text] <= avg_rate or tag_names[li.text] <= avg_tag_frequency:
+                    if len(recommended_problems) < 5:
+                        problem_id = div.find("div", style="float: left;").find("a").get("href")
+                        build_link = "https://codeforces.com" + problem_id
+                        if build_link not in all_problems and build_link not in recommended_problems and len(recommended_problems) < 5:
+                            recommended_problems.append(build_link)
+
+
+                        if len(recommended_problems) == 5:
+                            valid_length = False
+                            break
+
+                if not valid_length:
+                    break
+
+        if not valid_length:
+            break
+    
+        
+for problem in recommended_problems:
+    print(problem)
+
+
 
